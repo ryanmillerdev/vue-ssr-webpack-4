e14f2cb4cf770a11bceff3843ffd336729f2eec5
Ryan Miller <ryan@ryanandrewmiller.com>
2018-04-15 09:06:13 -0700

    Utilize webpack-dev-middleware.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const Express = require('express')
-const fs = require('fs')
 const path = require('path')
 const VueServerRenderer = require('vue-server-renderer')
 const Webpack = require('webpack')
+const WebpackDevMiddleware = require('webpack-dev-middleware')
 const WebpackHotMiddleware = require('webpack-hot-middleware')
 
 const [ webpackConfigServer, webpackConfigWeb ] = require('./webpack.config')
@@ -11,7 +11,11 @@ const app = Express()
 
 const isProduction = process.env.NODE_ENV === 'production'
 
-const generateRenderer = () => {
+let renderer = null
+
+const generateRenderer = (fs) => {
+  if (!fs) fs = require('fs')
+
   const serverBundlePath = path.resolve(__dirname, './public/vue-ssr-server-bundle.json')
   const clientBundlePath = path.resolve(__dirname, './public/vue-ssr-client-manifest.json')
   const templatePath = path.resolve(__dirname, './index.template.html')
@@ -20,35 +24,60 @@ const generateRenderer = () => {
     JSON.parse(fs.readFileSync(serverBundlePath, 'utf-8')),
     { 
       clientManifest: JSON.parse(fs.readFileSync(clientBundlePath, 'utf-8')),
-      template: fs.readFileSync(templatePath, 'utf-8')
+      // Always read the HTML template from the filesystem.
+      template: require('fs').readFileSync(templatePath, 'utf-8')
     }
   )
 }
 
-let renderer = generateRenderer()
-
-if (!isProduction) {
+if (isProduction) {
+  renderer = generateRenderer()
+} else {
   const webCompiler = Webpack(webpackConfigWeb)
   const serverCompiler = Webpack(webpackConfigServer)
 
-  app.use(WebpackHotMiddleware(webCompiler, { heartbeat: 1000, log: false }))
-
-  webCompiler.watch({}, (err, stats) => {
-    console.info(`*** WEB COMPILATION COMPLETE ***`)
-    console.info(Object.keys(stats.compilation.assets))
-    renderer = generateRenderer()
+  const devMiddleware = WebpackDevMiddleware(webCompiler, { 
+    logLevel: 'warn',
+    publicPath: webpackConfigWeb.output.publicPath 
+  })
+  const hotMiddleware = WebpackHotMiddleware(webCompiler, { 
+    heartbeat: 1000, 
+    log: false 
   })
 
-  serverCompiler.watch({}, (err, stats) => {
-    console.info(`*** SERVER COMPILATION COMPLETE ***`)
-    console.info(Object.keys(stats.compilation.assets))
-    renderer = generateRenderer()
+  app.use(devMiddleware)
+  app.use(hotMiddleware)
+
+  serverCompiler.outputFileSystem = devMiddleware.fileSystem
+
+  // The webpack-dev-middleware will automatically build the assets for our web entry point.
+  // Thus, we don't need to call `watch` on the above webCompiler. We do, however, need to 
+  // call watch our serverCompiler to ensure that both the server and client stay in sync.
+  webCompiler.hooks.afterEmit.tap('Web Compilation', (stats) => {
+    console.info(`*** WEB COMPILATION COMPLETE ***\n`)
+    console.group('Generated Assets')
+    Object.keys(stats.assets).forEach(a => console.info(a))
+    console.groupEnd()
+
+    serverCompiler.run((err, stats) => {
+      console.info(`\n*** SERVER COMPILATION COMPLETE *** \n`)
+      console.group('Generated Assets')
+      Object.keys(stats.compilation.assets).forEach(a => console.info(a))
+      console.groupEnd()
+
+      renderer = generateRenderer(devMiddleware.fileSystem)
+    })
   })
 }
 
 app.use('/public/', Express.static('public'))
 
 app.get('*', (req, res) => { 
+  if (!renderer) {
+    res.status(500).send('Renderer is being created, one moment....')
+    return
+  }
+
   renderer.renderToStream({ url: req.url }).on('error', (err) => {
     if (err.message === '404') {
       res.status(404).send()
